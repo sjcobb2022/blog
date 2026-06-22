aebd7095c5e3a5176233cb73178b3012f214c9f9
Samuel Cobb <sjcobb2003@gmail.com>
2026-06-22 23:49:44 +0100

    prep static

--- vite.config.ts
+++ vite.config.ts
@@ -16,12 +16,17 @@ export default defineConfig({
 				runes: ({ filename }) =>
 					filename.split(/[/\\]/).includes('node_modules') ? undefined : true
 			},
-			adapter: adapter(),
+			adapter: adapter({
+				fallback: '404.html'
+			}),
+                        paths: {
+                                base: process.argv.includes('dev') ? '' : (process.env.BASE_PATH as `/${string}`),
+                        },
 			preprocess: [
 				mdsvex({
 					extensions: ['.svx', '.md'],
 					remarkPlugins: [remarkMath],
-					/* @ts-ignore */
+					// @ts-expect-error weird plugin behaviour
 					rehypePlugins: [rehypeSlug, rehypeAutolinkHeadings, rehypeKatexSvelte]
 				})
 			],
