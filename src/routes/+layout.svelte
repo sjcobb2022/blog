7fe6a89b333593052d0b85c3c988a4fd93c80c3d
Samuel Cobb <sjcobb2003@gmail.com>
2026-06-22 20:16:14 +0100

    work on layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,8 +4,33 @@
 	let { children } = $props();
 </script>
 
+<style>
+	nav {
+		font-size: 1.4rem;
+	}
+
+	nav a {
+		margin-right: 1.5rem;
+	}
+</style>
+
 <svelte:head>
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
+
+<header>
+	<nav>
+		<a href="/">Home</a>
+		<a href="/about">About</a>
+		<a href="/projects">Projects</a>
+		<a href="/contact">Contact</a>
+	</nav>
+</header>
+
 {@render children()}
+
+
+<footer>
+        <!-- TODO: Get a nice header. Add to nix chain? -->
+</footer>
