99b05fbce754aea95d38b4a3bfcc3905c1129ee1
Samuel Cobb <sjcobb2003@gmail.com>
2026-06-23 01:48:21 +0100

    add header clicking

--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -24,6 +24,20 @@
 </article>
 
 <style>
+	:global(html) {
+		scroll-behavior: smooth;
+	}
+	
+	article :global(:is(h1, h2, h3, h4, h5, h6)) {
+		position: relative;
+	}
+
+	article :global(:is(h1, h2, h3, h4, h5, h6) a[aria-hidden]) {
+		position: absolute;
+		inset: 0;
+		cursor: text;
+	}
+
 	article {
 		counter-reset: sidenote-counter;
 	}
